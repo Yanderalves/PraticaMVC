abd7f4289475bae6da3d9406fdeb06607add805f
Yander Alves <yanderceara@gmail.com>
2023-01-07 13:59:56 -0300

    Subindo as Migrations

--- Migrations/20230107011551_InitialCreate.Designer.cs
+++ Migrations/20230107011551_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SiteMVC.Data;
 
@@ -10,9 +11,10 @@ using SiteMVC.Data;
 namespace SiteMVC.Migrations
 {
     [DbContext(typeof(BancoContext))]
-    partial class BancoContextModelSnapshot : ModelSnapshot
+    [Migration("20230107011551_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
